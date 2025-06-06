4260be3d9f67e0f9d41833846ab7b0d2230e1017
ChandraShekharSaini <chandrashekharsaini322@gmail.com>
2025-06-06 08:17:58 +0530

    Added Query Mail

--- Nodemailer/SignupMailer.js
+++ Nodemailer/SignupMailer.js
@@ -34,7 +34,7 @@ const SignupMailer = async (email, randomName, randomPassword) => {
           "api-key": process.env.BREVO_API_KEY,
           "Content-Type": "application/json",
         },
-        timeout: 20000,
+        timeout: 40000,
       }
     );
 
