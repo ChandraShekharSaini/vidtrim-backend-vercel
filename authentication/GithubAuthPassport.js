b92c985407a087314877c1f357f8c4812b22cc63
ChandraShekharSaini <chandrashekharsaini322@gmail.com>
2025-06-07 19:14:15 +0530

    added

--- authentication/GithubAuthPassport.js
+++ authentication/GithubAuthPassport.js
@@ -1,48 +1,46 @@
-import { Strategy as GitHubStrategy } from "passport-github2"
-import passport from "passport"
-import User from "../models/user.models.js"
-import { generateRandomName, generateRandomPassword } from "../utilits/functionalHandler.js"
-passport.use(new GitHubStrategy({
-    clientID: process.env.GITHUB_CLIENT_ID,
-    clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    callbackURL: process.env.GITHUB_CALLBACK_URL,
-},
+import { Strategy as GitHubStrategy } from "passport-github2";
+import passport from "passport";
+import User from "../models/user.models.js";
+import {
+  generateRandomName,
+  generateRandomPassword,
+} from "../utilits/functionalHandler.js";
+passport.use(
+  new GitHubStrategy(
+    {
+      clientID: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      callbackURL: process.env.GITHUB_CALLBACK_URL,
+    },
     async function (accessToken, refreshToken, profile, done) {
+   
 
+      const generatedUsername = generateRandomName();
+      const generatedPassword = generateRandomPassword();
 
-        const generatedUsername = generateRandomName()
-        const generatedPassword = generateRandomPassword()
+      try {
+        const user = await User.findOne({ email: profile.emails[0].value });
 
-
-        try {
-            const user = await User.findOne({ email: profile.emails[0].value });
-
-            if (user) {
-                return done(null, user);
-            }
-
-            const newUser = await User.create({
-                firstName: profile.name.displayName,
-                username: generatedUsername,
-                password: generatedPassword,
-                email: profile.emails[0].value,
-                profilePicture: {
-                    githubImageUrl: profile.photos[0].value,
-                }
-            })
-
-            return done(null, newUser);
-
-
-
-
-        } catch (error) {
-            console.log(error)
+        if (user) {
+          return done(null, user);
         }
 
+        const newUser = await User.create({
+          firstName: profile.displayName,
+          username: generatedUsername,
+          password: generatedPassword,
+          email: profile.emails[0].value,
+          profilePicture: {
+            githubImageUrl: profile.photos[0].value,
+          },
+        });
 
+        return done(null, newUser);
+      } catch (error) {
+        console.log(error);
+      }
     }
-));
+  )
+);
 
-
-export default passport
+export default passport;
